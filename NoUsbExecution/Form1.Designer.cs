0a8bedcc3cee22117ff21ad006aebaaed25ef959
emreyildiz1980 <emreyildiz1980@hotmail.com>
2019-03-25 20:09:39 +0300

    Initial Commit

---
+++ NoUsbExecution/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace NoUsbExecution
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fullaccessbtn = new System.Windows.Forms.Button();
+            this.readonlybtn = new System.Windows.Forms.Button();
+            this.disabledbtn = new System.Windows.Forms.Button();
+            this.applybtn = new System.Windows.Forms.Button();
+            this.refreshbtn = new System.Windows.Forms.Button();
+            this.closebtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // fullaccessbtn
+            // 
+            this.fullaccessbtn.Location = new System.Drawing.Point(51, 80);
+            this.fullaccessbtn.Name = "fullaccessbtn";
+            this.fullaccessbtn.Size = new System.Drawing.Size(109, 50);
+            this.fullaccessbtn.TabIndex = 0;
+            this.fullaccessbtn.Text = "Full Access";
+            this.fullaccessbtn.UseVisualStyleBackColor = true;
+            // 
+            // readonlybtn
+            // 
+            this.readonlybtn.Location = new System.Drawing.Point(205, 80);
+            this.readonlybtn.Name = "readonlybtn";
+            this.readonlybtn.Size = new System.Drawing.Size(109, 50);
+            this.readonlybtn.TabIndex = 1;
+            this.readonlybtn.Text = "Read Only";
+            this.readonlybtn.UseVisualStyleBackColor = true;
+            // 
+            // disabledbtn
+            // 
+            this.disabledbtn.Location = new System.Drawing.Point(357, 80);
+            this.disabledbtn.Name = "disabledbtn";
+            this.disabledbtn.Size = new System.Drawing.Size(109, 50);
+            this.disabledbtn.TabIndex = 2;
+            this.disabledbtn.Text = "Disabled";
+            this.disabledbtn.UseVisualStyleBackColor = true;
+            // 
+            // applybtn
+            // 
+            this.applybtn.Location = new System.Drawing.Point(205, 265);
+            this.applybtn.Name = "applybtn";
+            this.applybtn.Size = new System.Drawing.Size(83, 29);
+            this.applybtn.TabIndex = 3;
+            this.applybtn.Text = "Apply";
+            this.applybtn.UseVisualStyleBackColor = true;
+            // 
+            // refreshbtn
+            // 
+            this.refreshbtn.Location = new System.Drawing.Point(312, 265);
+            this.refreshbtn.Name = "refreshbtn";
+            this.refreshbtn.Size = new System.Drawing.Size(83, 29);
+            this.refreshbtn.TabIndex = 4;
+            this.refreshbtn.Text = "Refresh";
+            this.refreshbtn.UseVisualStyleBackColor = true;
+            // 
+            // closebtn
+            // 
+            this.closebtn.Location = new System.Drawing.Point(423, 265);
+            this.closebtn.Name = "closebtn";
+            this.closebtn.Size = new System.Drawing.Size(83, 29);
+            this.closebtn.TabIndex = 5;
+            this.closebtn.Text = "Close";
+            this.closebtn.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(518, 324);
+            this.Controls.Add(this.closebtn);
+            this.Controls.Add(this.refreshbtn);
+            this.Controls.Add(this.applybtn);
+            this.Controls.Add(this.disabledbtn);
+            this.Controls.Add(this.readonlybtn);
+            this.Controls.Add(this.fullaccessbtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button fullaccessbtn;
+        private System.Windows.Forms.Button readonlybtn;
+        private System.Windows.Forms.Button disabledbtn;
+        private System.Windows.Forms.Button applybtn;
+        private System.Windows.Forms.Button refreshbtn;
+        private System.Windows.Forms.Button closebtn;
+    }
+}
+
---
+++ NoUsbExecution/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace NoUsbExecution
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            RegistryKey key;
+
+            // Looking for the "SYSTEM\\CurrentControlSet\\Control\\StorageDevicePolicies" value. If it doesn't exist, we catch the exception and try to create it.
+            try
+            {
+                key = Registry.LocalMachine.OpenSubKey("SYSTEM\\CurrentControlSet\\Control\\StorageDevicePolicies");
+
+                if (Convert.ToInt16(key.GetValue("WriteProtect", null)) == 1)
+                {
+                    readonlybtn.BackColor = Color.Green;
+                }
+                else
+                {
+                    fullaccessbtn.BackColor = Color.Green;
+                }
+            }
+            catch (NullReferenceException)
+            {
+
+                key = Registry.LocalMachine.OpenSubKey("SYSTEM\\CurrentControlSet\\Control", true);
+                key.CreateSubKey("StorageDevicePolicies");
+                key.Close();
+            }
+
+            catch (Exception) { }
+
+            // Trying to access the value of the "SYSTEM\CurrentControlSet\Services\UsbStor". If it doesn't exist, we create it. 
+            try
+            {
+                key = Registry.LocalMachine.OpenSubKey("SYSTEM\\CurrentControlSet\\Services\\UsbStor");
+
+                if (Convert.ToInt16(key.GetValue("Start", null)) == 4)
+                {
+                    disabledbtn.BackColor = Color.Green;
+                    return;
+                }
+            }
+
+            catch (NullReferenceException)
+            {
+                key = Registry.LocalMachine.OpenSubKey("SYSTEM\\CurrentControlSet\\Services", true);
+                key.CreateSubKey("USBSTOR");
+
+                key.CreateSubKey("SYSTEM\\CurrentControlSet\\Services\\UsbStor", true);
+
+                key.SetValue("Type", 1, RegistryValueKind.DWord);
+                key.SetValue("Start", 3, RegistryValueKind.DWord);
+                key.SetValue("ImagePath", "system32\\drivers\\usbstor.sys", RegistryValueKind.ExpandString);
+                key.SetValue("ErrorControl", 1, RegistryValueKind.DWord);
+                key.SetValue("DisplayName", "Usb Mass Storage Driver", RegistryValueKind.String);
+                key.Close();
+            }
+
+            catch (Exception) { }
+
+            void UsbEnableWriteProtect()
+            {
+                key = Registry.LocalMachine.OpenSubKey
+
+     ("SYSTEM\\CurrentControlSet\\Control\\StorageDevicePolicies", true);
+
+                if (key == null)
+
+                {
+
+                    Registry.LocalMachine.CreateSubKey
+
+        ("SYSTEM\\CurrentControlSet\\Control\\StorageDevicePolicies", RegistryKeyPermissionCheck.ReadWriteSubTree);
+
+                    key = Registry.LocalMachine.OpenSubKey
+
+         ("SYSTEM\\CurrentControlSet\\Control\\StorageDevicePolicies", true);
+
+                    key.SetValue("WriteProtect", 1, RegistryValueKind.DWord);
+                }
+                else if (key.GetValue("WriteProtect") != (object)(1))
+
+                {
+
+                    key.SetValue("WriteProtect", 1, RegistryValueKind.DWord);
+
+                }
+            }
+
+            void UsbDisableWriteProtect()
+            {
+                key = Registry.LocalMachine.OpenSubKey
+
+          ("SYSTEM\\CurrentControlSet\\Control\\StorageDevicePolicies", true);
+
+                if (key != null)
+
+                {
+
+                    key.SetValue("WriteProtect", 0, RegistryValueKind.DWord);
+
+                }
+
+                key.Close();
+            }
+
+            void UsbDisableAllStorageDevices()
+            {
+                key = Registry.LocalMachine.OpenSubKey
+
+                ("SYSTEM\\CurrentControlSet\\Services\\UsbStor", true);
+
+                if (key != null)
+
+                {
+
+                    key.SetValue("Start", 4, RegistryValueKind.DWord);
+
+                }
+
+                key.Close();
+            }
+
+            void UsbEnableAllStorageDevices()
+            {
+                key = Registry.LocalMachine.OpenSubKey
+
+                ("SYSTEM\\CurrentControlSet\\Services\\UsbStor", true);
+
+                if (key != null)
+
+                {
+
+                    key.SetValue("Start", 3, RegistryValueKind.DWord);
+
+                }
+
+                key.Close();
+            }
+        }
+    }
+}
